2b8cd978ddca37a74efe62d682c23c9c075d6783
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-11-19 15:27:03 +0000

    Added Controllers for Notifications, Events and Artists

---
+++ app/Http/Controllers/Notifications/RestrictionNotificationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\RestrictionNotification;
+
+class RestrictionNotificationController extends Controller
+{
+    
+}
