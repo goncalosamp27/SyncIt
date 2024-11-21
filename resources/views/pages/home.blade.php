2b079d209f86fc8212553bc6b47706d249cde99a
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-11-21 14:27:14 +0000

    slogan box created

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,37 @@
 
 @section('content')
 	<div class="slogan-container">
-		<div class="slogan-container-content">
+
+		<div class="slogan-container-text">
 			<h1>Create <span class="highlighter">Your Own Show!</span></h1>
 			<h2><span class="highlighter">Steal the Spotlight</span></h2>
+			<p>And <span class="highlighter">Show</span> the World your <span class="highlighter">Talent</span></p>
+
+			@if (Auth::check())
+				<a class="take-me-button" href="{{ url('/create') }}">Get Started!</a>
+			@else
+				<a class="take-me-button" href="{{ url('/login') }}">Get Started!</a>
+			@endif	
 		</div>
+
+		<div class="slogan-container-image">
+			<img src="https://c4.wallpaperflare.com/wallpaper/380/24/860/dj-turntable-purple-music-wallpaper-preview.jpg" alt="Placeholder">
+		</div>
+	</div>
+
+	<div class="purple-line"></div>
+
+	<div class="future-events">
+		<h1>Future Events: </h1>
+	</div>
+	
+
+	<div class="past-events">
+		<h1>Past Events: </h1>
+	</div>
+
+
+	<div class="artists">
+		<h1>Artists: </h1>
 	</div>
 @endsection	
