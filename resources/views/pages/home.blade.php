d5c3cff3202f5ed00b28522054477ec03356a1d8
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-11-21 16:18:52 +0000

    minor css changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 	<div class="slogan-container">
-
 		<div class="slogan-container-text">
 			<h1>Create <span class="highlighter">Your Own Show!</span></h1>
 			<h2><span class="highlighter">Steal the Spotlight</span></h2>
@@ -14,25 +13,28 @@
 				<a class="take-me-button" href="{{ url('/login') }}">Get Started!</a>
 			@endif	
 		</div>
-
 		<div class="slogan-container-image">
 			<img src="https://c4.wallpaperflare.com/wallpaper/380/24/860/dj-turntable-purple-music-wallpaper-preview.jpg" alt="Placeholder">
 		</div>
 	</div>
 
-	<div class="purple-line"></div>
+	<div class="home-page-bottom">
+		<div class="purple-line"></div>
 
-	<div class="future-events">
-		<h1>Future Events: </h1>
-	</div>
-	
+		<div class="future-events">
+			<h1>Future Events: </h1>
+		</div>
 
-	<div class="past-events">
-		<h1>Past Events: </h1>
-	</div>
+		<div class="purple-line"></div>
 
+		<div class="past-events">
+			<h1>Past Events: </h1>
+		</div>
 
-	<div class="artists">
-		<h1>Artists: </h1>
-	</div>
+		<div class="purple-line"></div>
+
+		<div class="artists">
+			<h1>Artists: </h1>
+		</div>
+	</div>	
 @endsection	
