b0b33a790bb53e9b1135497255c7ef36221c1831
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-11-21 13:17:56 +0000

    routes edited, now localhost redirects to homepage

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="slogan-container">
+		<div class="slogan-container-content">
+			<h1>Create <span class="highlighter">Your Own Show!</span></h1>
+			<h2><span class="highlighter">Steal the Spotlight</span></h2>
+		</div>
+	</div>
+@endsection	
