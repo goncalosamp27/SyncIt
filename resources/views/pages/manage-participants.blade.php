8d638251ddacc168540d390b864a5af68bba5c19
Rui Xavier Costa da Silva <xavier.silva@Ruis-MBP.lan>
2024-11-23 22:56:40 +0000

    adding js to participants page

--- resources/views/pages/manage-participants.blade.php
+++ resources/views/pages/manage-participants.blade.php
@@ -2,8 +2,24 @@
 
 @section('content')
     <div class="admin_page">
-        <h1> Participants </h1>
+    <div class="participants-header">
+    <h1>Participants</h1>
+    <div class="add-participant-container">
+        <button class="add-participant-btn" onclick="toggleSearchBar()">➕</button>
+        <div id="search-bar-container" class="search-bar-container">
+            <input 
+                type="text" 
+                id="participant-search" 
+                placeholder="Search username..." 
+                class="search-bar-input"
+            />
+            <button class="search-submit-btn">Invite</button>
+        </div>
+    </div>  
+    </div>
 
+
+        
         <div class="search-bar">
             <button type="submit" class="search-button">🔍</button>
             <input 
@@ -22,8 +38,14 @@
                     <h3 class="member-name">{{ $participant->display_name }}</h3>
                     <p class="member-username">{{'@' . $participant->username }}</p>
                 </div> 
-
+                <div class="member-edit">
+                    <button class="remove-participant" title="Remove participant">
+                        Remove
+                    </a>
+                </div>
             </div>
+
         @endforeach
+
     </div>
 @endsection
