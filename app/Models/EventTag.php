08b4acb56ddc46311b96ef9ccab900e8399d4104
Sofi <sofia@L-PF38LPFZ.criticaltechworks.com>
2024-11-24 15:08:13 +0000

    Implemented Tags adding to db

--- app/Models/EventTag.php
+++ app/Models/EventTag.php
@@ -11,8 +11,24 @@ class EventTag extends Model
 
     protected $table = 'event_tag';
 
+    protected $primaryKey = null;
+    public $incrementing = false;
     //by default primary key will be incremented
 
     public $timestamps = false;
+
+    protected $fillable = [
+        'event_id',
+        'tag_id'
+    ];
+    public static function createEventTag(int $eventId, int $tagId)
+    {
+        $eventTag = new self();
+
+        $eventTag->event_id = $eventId;
+        $eventTag->tag_id = $tagId;
+
+        $eventTag->save();
+    }
 
 }
