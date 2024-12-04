fd51b41b04dd8fca0e1f49b1dd51f740ae438af4
daunisiaj <daunisia.j@gmail.com>
2024-12-04 03:33:30 +0000

    fixed fetch

--- resources/views/pages/comment.blade.php
+++ resources/views/pages/comment.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/comment.js') }}" defer></script>
 
     <div class="event-page-content">
         <div class="event-page-info">
