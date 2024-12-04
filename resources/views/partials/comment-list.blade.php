8b603d68b2271c66d84242e94ee32388f4e8b049
daunisiaj <daunisia.j@gmail.com>
2024-12-04 04:20:02 +0000

    fixed comment count

--- resources/views/partials/comment-list.blade.php
+++ resources/views/partials/comment-list.blade.php
@@ -1,9 +1,11 @@
+<h1>{{ isset($comments) && is_countable($comments) ? count($comments) : 0 }} Comments:</h1>
 @foreach($comments as $comment)
     <div class="comment">
         <strong>{{ $comment->member->username }}</strong>: {{ $comment->text }}
         <small>{{ $comment->comment_date }}</small>
     </div>
 @endforeach
+
 
 @if($comments->isEmpty())
     <p>No comments yet.</p>
