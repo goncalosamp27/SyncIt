47d9b279ae6896222754b3d3cd5b4cbf4208a612
goncalosamp27 <goncalo20.sampaio03@gmail.com>
2024-12-16 11:39:55 +0000

    starting to refactor buying tickets

--- database/thingy-seed.sql
+++ database/thingy-seed.sql
@@ -68,7 +68,7 @@ CHECK (
 CREATE DOMAIN name_domain AS VARCHAR(50)
 CHECK (
     CHAR_LENGTH(VALUE) BETWEEN 3 AND 50 
-    AND VALUE ~ '^[A-Za-z0-9_ ]+$'
+    AND VALUE ~ '^[A-Za-z0-9._\- ]+$'
 );
 
 CREATE DOMAIN password_domain AS VARCHAR(100)
@@ -197,6 +197,7 @@ CREATE TABLE ticket (
     event_id INT NOT NULL,
     ticket_date TIMESTAMP NOT NULL CHECK (ticket_date >= CURRENT_DATE),
     member_id INT NOT NULL,
+    owner name_domain NOT NULL,
     FOREIGN KEY (event_id) REFERENCES event(event_id) ON DELETE CASCADE,
     FOREIGN KEY (member_id) REFERENCES member(member_id) ON DELETE CASCADE
 );
@@ -706,27 +707,6 @@ AFTER INSERT ON invitation
 FOR EACH ROW
 EXECUTE FUNCTION delete_join_requests_on_invitation();
 
-/*
-CREATE OR REPLACE FUNCTION notify_event_tomorrow()
-RETURNS TRIGGER AS $$
-BEGIN
-    -- Insert notifications for ticket holders
-    INSERT INTO notification (notification_message, notification_date, member_id)
-    SELECT 'The event you signed for is tomorrow! Don''t miss it!', CURRENT_TIMESTAMP, member_id
-    FROM ticket
-    WHERE ticket.event_id = NEW.event_id;
-
-    -- Link notifications to the event
-    INSERT INTO event_notification (notification_id, event_id)
-    SELECT n.notification_id, NEW.event_id
-    FROM notification n
-    WHERE n.notification_date = CURRENT_TIMESTAMP;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-*/
-
 -- Create the function for handling event status change notifications
 CREATE OR REPLACE FUNCTION notify_event_status_change()
 RETURNS TRIGGER AS $$
@@ -767,6 +747,27 @@ FOR EACH ROW
 WHEN (OLD.event_status = 'Active' AND NEW.event_status = 'Cancelled')
 EXECUTE FUNCTION notify_event_status_change();
 
+
+/*
+CREATE OR REPLACE FUNCTION notify_event_tomorrow()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Insert notifications for ticket holders
+    INSERT INTO notification (notification_message, notification_date, member_id)
+    SELECT 'The event you signed for is tomorrow! Don''t miss it!', CURRENT_TIMESTAMP, member_id
+    FROM ticket
+    WHERE ticket.event_id = NEW.event_id;
+
+    -- Link notifications to the event
+    INSERT INTO event_notification (notification_id, event_id)
+    SELECT n.notification_id, NEW.event_id
+    FROM notification n
+    WHERE n.notification_date = CURRENT_TIMESTAMP;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+*/
 
 INSERT INTO member (username, display_name, email, password, bio, profile_pic_url, member_status)
 VALUES 
@@ -1263,34 +1264,35 @@ VALUES
     (50, 2);         -- Dance
 
 
-
-INSERT INTO ticket (event_id, ticket_date, member_id)
+INSERT INTO ticket (event_id, ticket_date, member_id, owner)
 VALUES -- Tickets for different events
-    (1, NOW(), 3),    -- Member 3 has a ticket for Salsa Night Fever
-    (2, NOW(), 5),    -- Member 5 has a ticket for Tech Beats Bash
-    (3, NOW(), 8),    -- Member 8 has a ticket for Classical Harmony
-    (4, NOW(), 10),   -- Member 10 has a ticket for DJ Night Live
-    (5, NOW(), 12),   -- Member 12 has a ticket for Reggae Beach Party
-    (6, NOW(), 15),   -- Member 15 has a ticket for Swing Dance Gala
-    (7, NOW(), 18),   -- Member 18 has a ticket for Metal Madness
-    (8, NOW(), 20),   -- Member 20 has a ticket for Violin Virtuoso
-    (9, NOW(), 22),   -- Member 22 has a ticket for Jazz Night
-    (10, NOW(), 25),  -- Member 25 has a ticket for Pop Fiesta
-    (21, NOW(), 28),  -- Member 28 has a ticket for Afrobeat Summer Jam
-    (22, NOW(), 30),  -- Member 30 has a ticket for Folk Fest
-    (27, NOW(), 32),  -- Member 32 has a ticket for Disco Fever
-    (38, NOW(), 35),  -- Member 35 has a ticket for Lo-Fi Chillout
-    (40, NOW(), 37),  -- Member 37 has a ticket for Hard Rock Havoc
-    (41, NOW(), 40),  -- Member 40 has a ticket for Ambient Chill
-    (42, NOW(), 45),  -- Member 45 has a ticket for Opera Under the Stars
-    (43, NOW(), 47),  -- Member 47 has a ticket for Country Fair
-    (44, NOW(), 50),  -- Member 50 has a ticket for Tribal Beats Night
-    (46, NOW(), 55),  -- Member 55 has a ticket for Psytrance Universe
-    (47, NOW(), 58),  -- Member 58 has a ticket for Flamenco Fire
-    (48, NOW(), 60),  -- Member 60 has a ticket for Neo Soul Groove
-    (49, NOW(), 62),  -- Member 62 has a ticket for Blues on the Bayou
-    (50, NOW(), 65),  -- Member 65 has a ticket for Bollywood Beats
-    (50, NOW(), 68);  -- Member 68 has a ticket for Indie Acoustic Evening
+    (1, NOW(), 3, 'John Cena'),               -- Member 3 has a ticket for Salsa Night Fever
+    (2, NOW(), 5, 'Travis Scott'),            -- Member 5 has a ticket for Tech Beats Bash
+    (3, NOW(), 8, 'Emily Watson'),            -- Member 8 has a ticket for Classical Harmony
+    (4, NOW(), 10, 'Michael Jordan'),         -- Member 10 has a ticket for DJ Night Live
+    (5, NOW(), 12, 'Sophia Brown'),           -- Member 12 has a ticket for Reggae Beach Party
+    (6, NOW(), 15, 'James Bond'),             -- Member 15 has a ticket for Swing Dance Gala
+    (7, NOW(), 18, 'Chris Hemsworth'),        -- Member 18 has a ticket for Metal Madness
+    (8, NOW(), 20, 'Dwayne Johnson'),         -- Member 20 has a ticket for Violin Virtuoso
+    (9, NOW(), 22, 'Ariana Grande'),          -- Member 22 has a ticket for Jazz Night
+    (10, NOW(), 25, 'Taylor Swift'),          -- Member 25 has a ticket for Pop Fiesta
+    (21, NOW(), 28, 'Elon Musk'),             -- Member 28 has a ticket for Afrobeat Summer Jam
+    (22, NOW(), 30, 'Billie Eilish'),         -- Member 30 has a ticket for Folk Fest
+    (27, NOW(), 32, 'Kanye West'),            -- Member 32 has a ticket for Disco Fever
+    (38, NOW(), 35, 'Selena Gomez'),          -- Member 35 has a ticket for Lo-Fi Chillout
+    (40, NOW(), 37, 'Drake Graham'),          -- Member 37 has a ticket for Hard Rock Havoc
+    (41, NOW(), 40, 'Megan Fox'),             -- Member 40 has a ticket for Ambient Chill
+    (42, NOW(), 45, 'LeBron James'),          -- Member 45 has a ticket for Opera Under the Stars
+    (43, NOW(), 47, 'Zendaya Coleman'),       -- Member 47 has a ticket for Country Fair
+    (44, NOW(), 50, 'Johnny Depp'),           -- Member 50 has a ticket for Tribal Beats Night
+    (46, NOW(), 55, 'Robert Downey Jr.'),     -- Member 55 has a ticket for Psytrance Universe
+    (47, NOW(), 58, 'Scarlett Johansson'),    -- Member 58 has a ticket for Flamenco Fire
+    (48, NOW(), 60, 'Chris Evans'),           -- Member 60 has a ticket for Neo Soul Groove
+    (49, NOW(), 62, 'Jennifer Lawrence'),     -- Member 62 has a ticket for Blues on the Bayou
+    (50, NOW(), 65, 'Rihanna Fenty'),         -- Member 65 has a ticket for Bollywood Beats
+    (50, NOW(), 68, 'Bruno Mars');            -- Member 68 has a ticket for Indie Acoustic Evening
+
+
 
 -- Creating Polls for events
 INSERT INTO poll (event_id, start_date, end_date)
