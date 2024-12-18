bf4d3aeaf630df95d1ff7b9994ff1f55277d04e7
Rui Xavier Costa da Silva <xavier.silva@Ruis-MBP.lan>
2024-12-18 16:01:45 +0000

    image access according to template

--- resources/views/partials/artist-card.blade.php
+++ resources/views/partials/artist-card.blade.php
@@ -1,9 +1,9 @@
 <a href="{{ route('artist', ['artist_id' => $artist->artist_id]) }}" class="artist-card" style="text-decoration: none; color: inherit;">
   <div class="artist-image">
     @can('isBanned', $artist->member)  
-      <img src="{{ asset('storage/profiles/default_user.png') }}" alt="Artist Image">
+      <img src="{{ asset('storage/profile/default_user.png') }}" alt="Artist Image">
     @else
-      <img src="{{ asset('storage/profiles/' . $artist->member->profile_pic_url) }}" alt="Artist Image">
+      <img src="{{ $artist->member->getProfileImage() }}" alt="Artist Image">
     @endcan 
   </div>
   <div class="artist-name">
